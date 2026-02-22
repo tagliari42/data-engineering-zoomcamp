1816d05f0f32398c8f35e7900656abb4b2b421d7
Rafael Tagliari <tagliarirafael@gmail.com>
2026-02-22 23:25:06 +0000

    refactor(docker): restructure compose setup and separate ingestion services

--- pipeline/ingest_data_3.py
+++ pipeline/ingest_data_3.py
@@ -5,22 +5,13 @@ import pandas as pd
 from sqlalchemy import create_engine
 import click
 
-dtype = {
-    "LocationID": "Int64",
-    "Borough": "string",
-    "Zone": "string",
-    "service_zone": "string",
-}
-
-import click
-
 @click.command()
 @click.option('--pg-user', default='root', help='PostgreSQL user')
 @click.option('--pg-pass', default='root', help='PostgreSQL password')
 @click.option('--pg-host', default='localhost', help='PostgreSQL host')
 @click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--target-table', default='zones', help='Target table name')
+@click.option('--target-table', default='green_taxi_trips', help='Target table name')
 
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
     pg_user = pg_user
@@ -31,28 +22,25 @@ def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
 
     target_table = target_table
 
-    url = 'https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv'
+    url = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet"
 
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-    df = pd.read_csv(
-        url,
-        dtype=dtype,
-    )
+    df = pd.read_parquet(url)
 
     first = True
 
-
     if first:
         df.head(0).to_sql(
-            name=target_table,
+            name= target_table,
             con=engine,
             if_exists='replace'
         )
+
     first = False
 
     df.to_sql(
-        name=target_table,
+        name= target_table,
         con=engine, 
         if_exists='append'
     )
